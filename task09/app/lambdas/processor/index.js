f87caea6355a39e40935e8e7ecd6bbc5b06e59a0
Harut <zaqaryanharut242gmail.com>
2024-08-14 20:27:54 +0400

    Complete

--- task09/app/lambdas/processor/index.js
+++ task09/app/lambdas/processor/index.js
@@ -7,7 +7,6 @@ AWSXRay.captureAWS(AWS);
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.target_table || "Weather";
-console.log("~~~Table Name from env~~~", tableName);
 
 export const handler = async (event) => {
   const eventId = uuidv4();
@@ -16,28 +15,8 @@ export const handler = async (event) => {
     const response = await axios.get(
       "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
     );
-    console.log("~~~RESPONSE from axios", response);
-    const forecastData = response.data;
-    console.log("~~~FORECAST from axios", forecastData);
 
-    console.log("~~~FORECAST latitude", forecastData.latitude);
-    console.log("~~~FORECAST latitude typeof", typeof forecastData.latitude);
-    console.log("~~~FORECAST longitude", forecastData.longitude);
-    console.log("~~~FORECAST longitude typeof", typeof forecastData.longitude);
-    console.log("~~~FORECAST gen_ms", forecastData.generationtime_ms);
-    console.log("~~~FORECAST gen_ms typeof", typeof forecastData.generationtime_ms);
-    console.log("~~~FORECAST utc_of_sec", forecastData.utc_offset_seconds);
-    console.log("~~~FORECAST utc_of_sec typeof", typeof forecastData.utc_offset_seconds);
-    console.log("~~~FORECAST timezone", forecastData.timezone);
-    console.log("~~~FORECAST timezone typeof", typeof forecastData.timezone);
-    console.log("~~~FORECAST timez_abbr", forecastData.timezone_abbreviation);
-    console.log("~~~FORECAST timez_abbr typeof", typeof forecastData.timezone_abbreviation);
-    console.log("~~~FORECAST eevation", forecastData.elevation);
-    console.log("~~~FORECAST eevation typeof", typeof forecastData.elevation);
-    console.log("~~~FORECAST hourly_units", forecastData.hourly_units);
-    console.log("~~~FORECAST hourly_units typeof", typeof forecastData.hourly_units);
-    console.log("~~~FORECAST hourly", forecastData.hourly);
-    console.log("~~~FORECAST hourly typeof", typeof forecastData.hourly);
+    const forecastData = response.data;
 
     const item = {
       id: eventId,
@@ -59,8 +38,6 @@ export const handler = async (event) => {
         },
       },
     };
-    console.log("~~~ITEM~~~", item);
-    console.log("~~~ITEM type~~~", typeof item);
 
     const req = await docClient
       .put({
